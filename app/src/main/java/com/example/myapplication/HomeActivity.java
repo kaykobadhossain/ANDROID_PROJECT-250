0114d64233878c43b29d450c1628bcee014acb2d
KAYKOBAD HOSSAIN TANJIL <108022284+kaykobadhossain@users.noreply.github.com>
2024-01-21 04:13:43 +0600

    LOGOUT CARD FUNCTIONALITY ADDED
    when click in logout activity it will clear the information stored in local memory/ shared memory and go to login page

--- app/src/main/java/com/example/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -1,8 +1,14 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,6 +16,21 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        SharedPreferences sharedpreferences =getSharedPreferences("shared_pres", Context.MODE_PRIVATE);
+        String username =sharedpreferences.getString("username","").toString();
+        Toast.makeText(getApplicationContext(),"Welcome "+username,Toast.LENGTH_SHORT).show();
+
+        CardView logout=findViewById(R.id.cardLogout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor=sharedpreferences.edit();
+                editor.clear();
+                editor.apply();
+                startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+            }
+        });
 
     }
 }
